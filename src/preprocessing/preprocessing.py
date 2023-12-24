38ed55e9d8723c8505588b0d63c7e3756cd4b64e
rnimisha <nimisaraut@gmail.com>
2023-12-24 17:38:16 +0545

    feat: remove stop words

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,5 +1,11 @@
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 from pyspark.sql import DataFrame
 from pyspark.sql.functions import col, count, isnan, rand, when
+
+nltk.download("stopwords")
+ps = PorterStemmer()
 
 
 def print_null_rows(data: DataFrame):
@@ -18,3 +24,17 @@ def shuffle_rows(data: DataFrame) -> DataFrame:
     shuffled = data.withColumn("rand", rand())
     shuffled = shuffled.orderBy("rand").drop("rand")
     return shuffled
+
+
+def remove_stop_words(text: str) -> str:
+    words = text.split()
+    stopwords_eng = stopwords.words("english")
+
+    without_stopwords = [x for x in words if x not in (stopwords_eng)]
+    return " ".join(without_stopwords)
+
+
+def stem_words(text: str) -> str:
+    words = text.split()
+    stemmed = [ps.stem(word) for word in words]
+    return " ".join(stemmed)
