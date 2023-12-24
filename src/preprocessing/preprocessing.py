a729f96aec5ba05e91ac94c0934daae785151754
rnimisha <nimisaraut@gmail.com>
2023-12-24 13:36:19 +0545

    feat: shuffle the order of data

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,5 +1,5 @@
-from pyspark.sql import DataFrame, SparkSession
-from pyspark.sql.functions import col, count, isnan, when
+from pyspark.sql import DataFrame
+from pyspark.sql.functions import col, count, isnan, rand, when
 
 
 def print_null_rows(data: DataFrame):
@@ -11,3 +11,10 @@ def print_null_rows(data: DataFrame):
 def drop_missing_text_rows(data: DataFrame) -> DataFrame:
     clean_data = data.na.drop(subset=["text"])
     return clean_data
+
+
+def shuffle_rows(data: DataFrame) -> DataFrame:
+    # generate row with random values
+    shuffled = data.withColumn("rand", rand())
+    shuffled = shuffled.orderBy("rand").drop("rand")
+    return shuffled
